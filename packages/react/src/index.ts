3558909b41a457c5b540953bf242265b817a69c0
Rasmus John Pedersen <mail@rjp.dk>
2019-12-23 10:11:21 +0100

    Re-export common types for easier access

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,5 @@
 export { default as Form } from './components/Form'
+
+export type FormData = import('./types').FormData
+export type UmbracoForm = import('./types').Form
+export type Theme = import('./types').Theme
